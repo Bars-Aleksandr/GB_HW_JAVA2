4f7e6f0ef092782e0e94cfd5ac32bf7cb18eec55
Aleksandr <bas8319@yahoo.com>
2023-05-29 13:15:07 +0300

    task2 for the Seminar 1

--- seminar1/task1/src/main/java/org/example/Task1.java
+++ seminar1/task1/src/main/java/org/example/Task1.java
@@ -1,10 +1,41 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 //Выбросить случайное целое число в диапазоне от 0 до 2000 и сохранить в i
+//Посчитать и сохранить в n номер старшего значащего бита выпавшего числа
+// Найти все кратные n числа в диапазоне от i до Short.MAX_VALUE сохранить в массив m1
+// Найти все некратные n числа в диапазоне от Short.MIN_VALUE до i и сохранить в массив m2
 public class Task1 {
     public static void main(String[] args) {
         int i = new Random().nextInt(2000);
+        System.out.println("Random number = " + i);
+        int n = findHighBitNumber(i);
+        System.out.println("MSb = " + n);
+    }
+
+    public static int findHighBitNumber(int dec) {
+
+        ArrayList arrBits = new ArrayList();
+        arrBits = intToBinRevers(dec);
+        int positionNum = arrBits.size()-1;
+        return positionNum;
+    }
+
+    private static ArrayList intToBinRevers(int i) {
+
+        ArrayList arrBins = new ArrayList();
+        int count = 0;
+        if (i !=0) {
+            while (i != 0) {
+                arrBins.add(i % 2);
+                i = i / 2;
+                count += 1;
+            }
+        } else {
+            arrBins.add(i);
+        }
+        return arrBins;
     }
 }
