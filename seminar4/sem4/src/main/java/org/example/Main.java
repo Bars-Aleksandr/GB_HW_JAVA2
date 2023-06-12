7f2b10c271a79269b1e72cfd8bf256a4b5082c8d
Aleksandr <bas8319@yahoo.com>
2023-06-12 12:14:45 +0300

    Task1 - reading from a file and output to the console

--- seminar4/sem4/src/main/java/org/example/Main.java
+++ seminar4/sem4/src/main/java/org/example/Main.java
@@ -1,33 +1,52 @@
 package org.example;
 
-import java.io.File;
+import java.io.*;
 import java.util.LinkedList;
-import java.util.Locale;
-import java.util.Scanner;
 
+//    1.Загрузить из файла многострочный текст формата ФИО возраст и пол через пробелы. Разбить по строкам и вывести в консоль в формате "Иванов И.И. 32 М"
+//    2.Загруженный и разбитый по строкам текст загрузить в подготовленные списки. Фамилии, имена, отчества, возрас и пол в отдельных списках.
+//    3.Отсортировать по возрасту используя дополнительный список индексов.
+//    4.Отсортировать по полу используя дополнительный список индексов.
+//    5.*Сортировка по полу и возрасту.
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите данные пользователя в формате (Иванов И.И. 32 М) или Q для выхода");
-        LinkedList<String> fname = new LinkedList<>();
-        LinkedList<String> sname = new LinkedList<>();
-        LinkedList<String> lname = new LinkedList<>();
-        LinkedList<Integer> age = new LinkedList<>();
-        LinkedList<Boolean> gender = new LinkedList<>();
-        LinkedList<Integer> id = new LinkedList<>();
-        while (true){
-            String in = scanner.nextLine();
-            if (in.toLowerCase().equals("q")) break;
-            String[] str = in.split(" ");
-            id.add(age.size());
-            fname.add(str[0]);
-            sname.add(str[1]);
-            lname.add(str[2]);
-            age.add(Integer.parseInt(str[3]));
-            gender.add(str[4].toLowerCase().contains("м"));
+    static LinkedList<Integer> id = new LinkedList<>();
+    static LinkedList<String> lastname = new LinkedList<>();
+    static LinkedList<String> firstname = new LinkedList<>();
+    static LinkedList<String> patronymic = new LinkedList<>();
+    static LinkedList<Integer> age = new LinkedList<>();
+    static LinkedList<String> gender = new LinkedList<>();
+    public static void main(String[] args) throws FileNotFoundException {
+        readDataFile();
+        writeDataConsole();
+
+
+    }
+
+    private static void writeDataConsole() {
+        String res;
+        for (int i = 0; i < id.size(); i++) {
+            System.out.println(lastname.get(i) + " " + firstname.get(i).toUpperCase().charAt(0) + ". " + patronymic.get(i).toUpperCase().charAt(0) + ". " + age.get(i) + " " + gender.get(i));
         }
-        for (int i = 0; i < age.size(); i++) {
-            System.out.println(fname.get(i)+ " " + sname.get(i) + " " + lname.get(i) + " ");
+    }
+
+    private static void readDataFile() throws FileNotFoundException {
+        try {
+            BufferedReader br = new BufferedReader( new FileReader("UserData.txt"));
+            String str;
+            String[] strArr;
+            while ((str = br.readLine()) != null){
+                strArr = str.split(" ");
+                id.add(lastname.size());
+                lastname.add(strArr[0]);
+                firstname.add(strArr[1]);
+                patronymic.add(strArr[2]);
+                age.add(Integer.valueOf(strArr[3]));
+                gender.add(strArr[4]);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
